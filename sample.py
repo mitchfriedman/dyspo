f068d61de8284d6d8b97ca6c67b052a60fa5d1ba
Mitchell Friedman <mitchfriedman5@gmail.com>
2018-08-26 14:04:09 -0700

    Update example to include endpoint dependency injection

--- sample.py
+++ sample.py
@@ -1,11 +1,27 @@
+import aiohttp
+
 from dyspo import Api
 from dyspo.endpoint import Endpoint
 from dyspo.resource import Get, Post
-from dyspo.response import Created, Ok
+from dyspo.response import Created, Ok, ServerError
+
+
+class ExternalService(object):
+    async def get_something(self):
+        async with aiohttp.ClientSession() as session:
+            resp = await session.get('https://google.com')
+            return resp
 
 
 class MyPostEndpoint(Endpoint):
+    def __init__(self, service):
+        self.service = service
+
     async def process(self, request):
+        resp = await self.service.get_something()
+        if resp.status != 200:
+            return ServerError({'message': 'Unable to fetch from external service'})
+
         return Created({'test': 'foo'})
 
 
@@ -19,7 +35,7 @@ if __name__ == '__main__':
 
     api.add_routes([
         Get(MyGetEndpoint(), '/foo'),
-        Post(MyPostEndpoint(), '/foo'),
+        Post(MyPostEndpoint(ExternalService()), '/foo'),
     ])
 
     api.run(port=5000, debug=True)
